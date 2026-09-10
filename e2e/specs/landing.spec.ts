79cd390e3ecd0e14f86744bfc78761b544304845
P J Borowiecki <hello@pjborowiecki.com>
2026-09-10 20:55:16 +0200

    🐛 fix(e2e): fix github workflow crash

--- e2e/specs/landing.spec.ts
+++ e2e/specs/landing.spec.ts
@@ -15,13 +15,19 @@ test.describe("landing page", () => {
     const line = landingPage.navigation().locator(':scope > div[aria-hidden="true"]')
     const viewportWidth = await page.evaluate(() => window.innerWidth)
 
-    for (const progress of [0.25, 0.75, 0.1, 0]) {
-      await page.evaluate((fraction) => {
-        const scrollHeight = document.documentElement.scrollHeight - window.innerHeight
-        window.scrollTo({ behavior: "instant", top: scrollHeight * fraction })
-      }, progress)
+    await page.waitForFunction(() => document.documentElement.scrollHeight > window.innerHeight)
 
-      await expect.poll(async () => (await line.boundingBox())?.width).toBeCloseTo(viewportWidth * progress, -1)
+    for (const progress of [0.25, 0.75, 0.1, 0]) {
+      await expect
+        .poll(async () => {
+          await page.evaluate((fraction) => {
+            const scrollHeight = document.documentElement.scrollHeight - window.innerHeight
+            window.scrollTo({ behavior: "instant", top: scrollHeight * fraction })
+          }, progress)
+
+          return (await line.boundingBox())?.width
+        })
+        .toBeCloseTo(viewportWidth * progress, -1)
       await expect(line).toHaveCSS("height", "1px")
     }
   })

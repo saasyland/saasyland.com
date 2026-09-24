pjborowiecki/saasy-land

📦 refactor(i18n): split messages into separate, targetted files for easier maintenance
Co-authored-by: Cursor <cursoragent@cursor.com>

scripts/check-i18n.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env bun
 
-import { readFileSync } from "node:fs"
+import { mkdtempSync, rmSync, writeFileSync } from "node:fs"
+import { tmpdir } from "node:os"
 import { join } from "node:path"
 import { $ } from "bun"
 
 import { I18N } from "../src/constants/_constants/i18n"
+import { getLocaleMessagesDir, loadLocaleMessagesFromDir } from "../src/integrations/next-intl/i18n.utils"
 
-const MESSAGES_DIR = join(import.meta.dir, "../src/integrations/next-intl/messages")
+const MESSAGES_DIR = getLocaleMessagesDir()
 const SOURCE_LOCALE = I18N.DEFAULT_LOCALE
+const MESSAGE_TYPES_PATH = join(import.meta.dirname, "../src/integrations/next-intl/en-US.d.json.ts")
 
 function compareAlphabetically(a: string, b: string): number {
   return a.localeCompare(b)
@@ -30,9 +33,14 @@ function flattenMessageKeys(value: unknown, prefix = ""): string[] {
 }
 
 function loadLocaleKeys(locale: string): Set<string> {
-  const filePath = join(MESSAGES_DIR, `${locale}.json`)
-  const json = JSON.parse(readFileSync(filePath, "utf8")) as unknown
-  return new Set(flattenMessageKeys(json))
+  const messages = loadLocaleMessagesFromDir(locale, MESSAGES_DIR)
+  return new Set(flattenMessageKeys(messages))
+}
+
+function writeMessageTypes(): void {
+  const messages = loadLocaleMessagesFromDir(SOURCE_LOCALE, MESSAGES_DIR)
+  const declaration = `// Auto-generated by check:i18n. Do not edit.\n\ndeclare const messages: ${JSON.stringify(messages, null, 2).trim()};\nexport default messages;\n`
+  writeFileSync(MESSAGE_TYPES_PATH, declaration, "utf8")
 }
 
 function checkLocaleParity(): boolean {
@@ -70,25 +78,38 @@ function checkLocaleParity(): boolean {
 
   process.stderr.write("\n❌ Translation locale parity check failed.\n\n")
   process.stderr.write(`${problems.join("\n")}\n\n`)
-  process.stderr.write(`Update ${MESSAGES_DIR} so every locale matches ${SOURCE_LOCALE}.json keys exactly.\n\n`)
+  process.stderr.write(`Update ${MESSAGES_DIR}/{locale}/*.json so every locale matches ${SOURCE_LOCALE} keys exactly.\n\n`)
   return false
 }
 
 async function checkInvalidMessages(): Promise<boolean> {
-  const result = await $`bunx --bun @lingual/i18n-check -l ${MESSAGES_DIR} -s ${SOURCE_LOCALE} -f next-intl -o invalidKeys`.quiet()
+  const tempDir = mkdtempSync(join(tmpdir(), "saasyland-i18n-check-"))
 
-  if (result.exitCode === 0) {
-    process.stdout.write("✅ Translation message syntax is valid in all locale files.\n")
-    return true
+  try {
+    for (const locale of I18N.LOCALES) {
+      const messages = loadLocaleMessagesFromDir(locale, MESSAGES_DIR)
+      writeFileSync(join(tempDir, `${locale}.json`), `${JSON.stringify(messages, null, 2)}\n`, "utf8")
+    }
+
+    const result = await $`bunx --bun @lingual/i18n-check -l ${tempDir} -s ${SOURCE_LOCALE} -f next-intl -o invalidKeys`.quiet()
+
+    if (result.exitCode === 0) {
+      process.stdout.write("✅ Translation message syntax is valid in all locale files.\n")
+      return true
+    }
+
+    process.stderr.write("\n❌ Invalid translation messages detected.\n\n")
+    process.stderr.write(result.stderr.toString())
+    process.stderr.write(result.stdout.toString())
+    return false
+  } finally {
+    rmSync(tempDir, { recursive: true, force: true })
   }
-
-  process.stderr.write("\n❌ Invalid translation messages detected.\n\n")
-  process.stderr.write(result.stderr.toString())
-  process.stderr.write(result.stdout.toString())
-  return false
 }
 
 async function run(): Promise<void> {
+  writeMessageTypes()
+
   const parityOk = checkLocaleParity()
   const invalidOk = await checkInvalidMessages()
 

saasyland/saasyland.com

Co-Authored-By: Cursor <cursoragent@cursor.com>

scripts/check-i18n.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env bun
 
-import { readFileSync } from "node:fs"
+import { writeFileSync } from "node:fs"
 import { join } from "node:path"
-import { $ } from "bun"
 
 import { I18N } from "../src/constants/_constants/i18n"
+import { getLocaleMessagesDir, loadLocaleMessagesFromDir } from "../src/integrations/next-intl/i18n.utils"
 
-const MESSAGES_DIR = join(import.meta.dir, "../src/integrations/next-intl/messages")
+const MESSAGES_DIR = getLocaleMessagesDir()
 const SOURCE_LOCALE = I18N.DEFAULT_LOCALE
+const MESSAGE_TYPES_PATH = join(import.meta.dirname, "../src/integrations/next-intl/en-US.d.json.ts")
 
 function compareAlphabetically(a: string, b: string): number {
   return a.localeCompare(b)
@@ -30,9 +31,14 @@ function flattenMessageKeys(value: unknown, prefix = ""): string[] {
 }
 
 function loadLocaleKeys(locale: string): Set<string> {
-  const filePath = join(MESSAGES_DIR, `${locale}.json`)
-  const json = JSON.parse(readFileSync(filePath, "utf8")) as unknown
-  return new Set(flattenMessageKeys(json))
+  const messages = loadLocaleMessagesFromDir(locale, MESSAGES_DIR)
+  return new Set(flattenMessageKeys(messages))
+}
+
+function writeMessageTypes(): void {
+  const messages = loadLocaleMessagesFromDir(SOURCE_LOCALE, MESSAGES_DIR)
+  const declaration = `// Auto-generated by check:i18n. Do not edit.\n\ndeclare const messages: ${JSON.stringify(messages, null, 2).trim()};\nexport default messages;\n`
+  writeFileSync(MESSAGE_TYPES_PATH, declaration, "utf8")
 }
 
 function checkLocaleParity(): boolean {
@@ -70,31 +76,16 @@ function checkLocaleParity(): boolean {
 
   process.stderr.write("\n❌ Translation locale parity check failed.\n\n")
   process.stderr.write(`${problems.join("\n")}\n\n`)
-  process.stderr.write(`Update ${MESSAGES_DIR} so every locale matches ${SOURCE_LOCALE}.json keys exactly.\n\n`)
+  process.stderr.write(`Update ${MESSAGES_DIR}/{locale}/*.json so every locale matches ${SOURCE_LOCALE} keys exactly.\n\n`)
   return false
 }
 
-async function checkInvalidMessages(): Promise<boolean> {
-  const result = await $`bunx --bun @lingual/i18n-check -l ${MESSAGES_DIR} -s ${SOURCE_LOCALE} -f next-intl -o invalidKeys`.quiet()
+function run(): void {
+  writeMessageTypes()
 
-  if (result.exitCode === 0) {
-    process.stdout.write("✅ Translation message syntax is valid in all locale files.\n")
-    return true
-  }
-
-  process.stderr.write("\n❌ Invalid translation messages detected.\n\n")
-  process.stderr.write(result.stderr.toString())
-  process.stderr.write(result.stdout.toString())
-  return false
-}
-
-async function run(): Promise<void> {
-  const parityOk = checkLocaleParity()
-  const invalidOk = await checkInvalidMessages()
-
-  if (!parityOk || !invalidOk) {
+  if (!checkLocaleParity()) {
     process.exit(1)
   }
 }
 
-await run()
+run()